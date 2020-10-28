f2ce81b0e565a5c682a34ab8157e166fd2c9b80d
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2020-10-28 11:19:05 +0200

    Add group struct entity

---
+++ core/model/group.go
@@ -0,0 +1,22 @@
+package model
+
+import "time"
+
+//Group represents group entity
+type Group struct {
+	ID                  string
+	Category            string //one of the enums categories list
+	Title               string
+	Privacy             string //public or private
+	Description         *string
+	ImageURL            *string
+	WebURL              *string
+	MembersCount        int //to be supported up to date
+	Tags                []string
+	MembershipQuestions []string
+
+	//TODO members
+
+	DateCreated time.Time
+	DateUpdated *time.Time
+}
