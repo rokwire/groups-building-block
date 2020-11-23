5889417889d578b0b07ef0f74f63247b2ab96773
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2020-11-23 09:21:48 +0200

    Prepare check group admins count

--- driven/storage/collection.go
+++ driven/storage/collection.go
@@ -219,7 +219,11 @@ func (collWrapper *collectionWrapper) AddIndex(keys interface{}, unique bool) er
 }
 
 func (collWrapper *collectionWrapper) Aggregate(pipeline interface{}, result interface{}, ops *options.AggregateOptions) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*15000)
+	return collWrapper.AggregateWithContext(context.Background(), pipeline, result, ops)
+}
+
+func (collWrapper *collectionWrapper) AggregateWithContext(ctx context.Context, pipeline interface{}, result interface{}, ops *options.AggregateOptions) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*15000)
 	defer cancel()
 
 	cursor, err := collWrapper.coll.Aggregate(ctx, pipeline, ops)
