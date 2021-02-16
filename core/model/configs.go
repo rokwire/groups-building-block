000e1b0c69030fefc5742c531c6393ac8ea86519
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-02-16 08:29:47 +0200

    Add update period field in the groups config entity

--- core/model/configs.go
+++ core/model/configs.go
@@ -2,5 +2,6 @@ package model
 
 //GroupsConfig represents the Groups configurations entity
 type GroupsConfig struct {
-	Name string `json:"name" bson:"name"`
+	Name         string `json:"name" bson:"name"`
+	UpdatePeriod int    `json:"update_period" bson:"update_period"`
 }
