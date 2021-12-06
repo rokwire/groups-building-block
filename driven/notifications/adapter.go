0313fe24e5eeb1bb831e5d9ea673c2343abc7bfc
Mladen Dryankov <mdryankov@gmail.com>
2021-12-06 12:10:52 +0200

    Don't send notifications to all users #89

--- driven/notifications/adapter.go
+++ driven/notifications/adapter.go
@@ -27,42 +27,43 @@ func NewNotificationsAdapter(internalAPIKey string, baseURL string) *Adapter {
 
 // SendNotification sends notification to a user
 func (na *Adapter) SendNotification(recipients []Recipient, topic *string, title string, text string, data map[string]string) error {
-	url := fmt.Sprintf("%s/api/int/message", na.baseURL)
+	if len(recipients) > 0 {
+		url := fmt.Sprintf("%s/api/int/message", na.baseURL)
 
-	bodyData := map[string]interface{}{
-		"priority":   10,
-		"recipients": recipients,
-		"topic":      topic,
-		"subject":    title,
-		"body":       text,
-		"data":       data,
+		bodyData := map[string]interface{}{
+			"priority":   10,
+			"recipients": recipients,
+			"topic":      topic,
+			"subject":    title,
+			"body":       text,
+			"data":       data,
+		}
+		bodyBytes, err := json.Marshal(bodyData)
+		if err != nil {
+			log.Printf("error creating notification request - %s", err)
+			return err
+		}
+
+		client := &http.Client{}
+		req, err := http.NewRequest("POST", url, bytes.NewReader(bodyBytes))
+		if err != nil {
+			log.Printf("error creating load user data request - %s", err)
+			return err
+		}
+		req.Header.Set("INTERNAL-API-KEY", na.internalAPIKey)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("error loading user data - %s", err)
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			log.Printf("error with response code - %d", resp.StatusCode)
+			return fmt.Errorf("error with response code != 200")
+		}
 	}
-	bodyBytes, err := json.Marshal(bodyData)
-	if err != nil {
-		log.Printf("error creating notification request - %s", err)
-		return err
-	}
-
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, bytes.NewReader(bodyBytes))
-	if err != nil {
-		log.Printf("error creating load user data request - %s", err)
-		return err
-	}
-	req.Header.Set("INTERNAL-API-KEY", na.internalAPIKey)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Printf("error loading user data - %s", err)
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		log.Printf("error with response code - %d", resp.StatusCode)
-		return fmt.Errorf("error with response code != 200")
-	}
-
 	return nil
 }
