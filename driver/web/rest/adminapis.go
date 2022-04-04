3ef96f29bc2f86868c70cfa4e7d0938f3b0f706f
Mladen Dryankov <mdryankov@gmail.com>
2022-04-04 15:14:24 +0300

    Uncomitted work for #128 (add PUT api and rename types)

--- driver/web/rest/adminapis.go
+++ driver/web/rest/adminapis.go
@@ -260,7 +260,7 @@ func (h *AdminApisHandler) GetGroupEvents(clientID string, current *model.User, 
 		return
 	}
 
-	events, err := h.app.Services.GetEvents(clientID, groupID)
+	events, err := h.app.Services.GetEvents(clientID, current, groupID, false)
 	if err != nil {
 		log.Printf("error getting group events - %s", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -345,7 +345,7 @@ func (h *AdminApisHandler) DeleteGroupEvent(clientID string, current *model.User
 		return
 	}
 
-	err := h.app.Services.DeleteEvent(clientID, *current, eventID, groupID)
+	err := h.app.Services.DeleteEvent(clientID, current, eventID, groupID)
 	if err != nil {
 		log.Printf("Error on deleting an event - %s\n", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
