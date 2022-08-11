e6afc574e846e57c80296fcc3d4dbc61bdbf06f7
Stephen Hurwit <sjhurwit@gmail.com>
2022-08-10 21:32:32 -0500

    update version to v1.5.64

--- docs/docs.go
+++ docs/docs.go
@@ -3608,7 +3608,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.5.63",
+	Version:          "1.5.64",
 	Host:             "localhost",
 	BasePath:         "/gr",
 	Schemes:          []string{"https"},
--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -43,7 +43,7 @@ type Adapter struct {
 
 // @title Rokwire Groups Building Block API
 // @description Rokwire Groups Building Block API Documentation.
-// @version 1.5.63
+// @version 1.5.64
 // @host localhost
 // @BasePath /gr
 // @schemes https
