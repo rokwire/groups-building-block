d237ad0d59df574ea3dfd4354245417fdc0946e6
Stefan Vitanov <stefan@inabyte.com>
2022-09-28 05:50:48 +0300

    remove externalID and email

--- core/model/member.go
+++ core/model/member.go
@@ -91,10 +91,8 @@ func (m GroupMembership) ToPublicMember() Member {
 	return Member{
 		ID:            m.ID,
 		UserID:        m.UserID,
-		ExternalID:    m.ExternalID,
 		Name:          m.Name,
 		NetID:         m.NetID,
-		Email:         m.Email,
 		PhotoURL:      m.PhotoURL,
 		Status:        status,
 		RejectReason:  m.RejectReason,
