39d1a7adfaca242bb1cc3946f7dbd0bfc97bcf44
Stephen Hurwit <sjhurwit@gmail.com>
2022-11-16 22:46:45 -0600

    update version to v1.7.0

--- docs/docs.go
+++ docs/docs.go
@@ -4166,7 +4166,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.6.1",
+	Version:          "1.7.0",
 	Host:             "localhost",
 	BasePath:         "/gr",
 	Schemes:          []string{"https"},
