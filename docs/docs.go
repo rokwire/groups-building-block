befae0ab7279a7eb26143ddaa0af261c80ed0a13
Mladen Dryankov <mdryankov@gmail.com>
2023-11-08 13:35:44 +0200

    Rebuild doc

--- docs/docs.go
+++ docs/docs.go
@@ -5218,7 +5218,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.20.0",
+	Version:          "1.27.2",
 	Host:             "localhost",
 	BasePath:         "/gr",
 	Schemes:          []string{"http"},
--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -47,7 +47,7 @@ type Adapter struct {
 
 // @title Rokwire Groups Building Block API
 // @description Rokwire Groups Building Block API Documentation.
-// @version 1.20.0
+// @version 1.27.2
 // @host localhost
 // @BasePath /gr
 // @schemes http
